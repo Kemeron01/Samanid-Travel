00d11e3235b92c5e8ed2223cbd798d8782c6a73a
Kemeron01 <kamronvladimirov00@gmail.com>
2025-10-04 17:41:54 +0500

    first run

--- src/schemas.py
+++ src/schemas.py
@@ -4,7 +4,7 @@ class UserCreate(BaseModel):
     full_name: str
     phone_number: str | None=None
     email: EmailStr
-    password: constr(min_length=8, max_length=72)
+    password: constr(min_length=8, max_length=128)
 
 class UserResponse(BaseModel):
     id: int
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,12 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+pwd_context = CryptContext(schemes=['pbkdf2_sha256'], deprecated='auto')
 
 def hash_password(password: str) -> str:
-    #return pwd_context.hash(password)
-    password_bytes = password.encode("utf-8")[:72]
-    return pwd_context.hash(password_bytes.decode("utf-8", errors="ignore"))
+    
+    return pwd_context.hash(password)
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    password_bytes = plain_password.encode("utf-8")[:72]
-    return pwd_context.verify(password_bytes.decode("utf-8", errors="ignore"), hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
