dca82967ad694d591616f6921c02c5bee12d65ae
Kemeron01 <kamronvladimirov00@gmail.com>
2025-11-24 10:53:40 +0500

    file change in auth-added dependencies and service py files. celery_tasks, config, mail, middleware file for atokenization and configuration

---
+++ src/auth/dependencies.py
@@ -0,0 +1,72 @@
+from typing import Any, List
+
+from fastapi import Depends, Request
+from fastapi.security import HTTPBearer
+from fastapi.security.http import HTTPAuthorizationCredentials
+from sqlmodel.ext.asyncio.session import AsyncSession 
+
+from src.db.main import get_sesion 
+from src.db.models import User
+from src.db.redis import token_in_blocklist
+
+from .service import UserService
+from .utils import decode_token
+from src.errors import (
+    InvalidToken, 
+    RefreshTokenRequired, 
+    AccessTokenRequired, 
+    AccountNotVerified
+)
+
+user_service = UserService()
+
+class TokenBearer(HTTPBearer):
+    def __init__(slef, auto_error = True):
+        super().__init__(auto_error=auto_error)
+        
+    async def __call__(self, request: Request) -> HTTPAuthorizationCredentials | None:
+        creds = await super.__call__(request)
+        
+        token = creds.credentials
+        
+        token_data = decode_token (token)
+         
+        if not self.token_valid(token):
+            raise InvalidToken()
+        
+        if await token_in_blocklist(token_data["jti"]):
+            raise InvalidToken
+        
+        self.verify_token_data(token_data)
+        
+        return token_data
+    
+    def token_valid(self, token:str) -> bool:
+        token_data = decode_token(token)
+        
+        return token_data is not None
+   
+    def verify_token_data(self, token_data):
+         
+        raise NotImplementedError("please override this method in child class")
+    
+    
+class AccessTokenBearer(TokenBearer):
+    def verify_token_data (self, token_data: dict) -> None:
+        if token_data and token_data["refresh"]:
+            raise AccessTokenRequired()
+
+class RefreshTokenBearer(TokenBearer):
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and token_data["refresh"]:
+            raise RefreshTokenBearer()
+        
+async def get_current_user(token_details: dict =Depends(AccessTokenBearer()), session: AsyncSession = Depends(get_sesion)):
+    user_email = token_details["user"]["email"]
+    
+    user = await user_service.get_user_by_email(user_email, session)
+    
+    return user
+
+class RoleChecker:
+    pass
--- src/db/main.py
+++ src/db/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from .import models, database
 from ..auth.routes import router as auth_router
+from src.config import Config 
+from sqlmodel import SQLModel
+
+from typing import AsyncGenerator
+from sqlalchemy.exc.asyncio import AsyncSession, create_async_engine, async_sessionmaker 
+
+
 
 models.Base.metadata.create_all(bind=database.engine)
 
